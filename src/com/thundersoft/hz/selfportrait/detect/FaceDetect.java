e072f726ef53ebda15f3b8afe20ed6cdbce2ea91
Alex Naidis <alex.naidis@linux.com>
2017-05-12 16:22:52 +0000

    Revert "FaceDetect: Catch linker errors during initialization"
    Upstream fix incoming.
    
    This reverts commit 0b8309885e6b1a5c46799fda826915d34b23e641.
    
    Change-Id: I4c1c6b27a2654f9c3af2f9554160c43ad1d622ae

--- src/com/thundersoft/hz/selfportrait/detect/FaceDetect.java
+++ src/com/thundersoft/hz/selfportrait/detect/FaceDetect.java
@@ -39,12 +39,7 @@ public class FaceDetect {
      * initialize method,MUST called at first time.
      */
     public void initialize() {
-        try {
-            mHandle = native_create();
-        } catch (UnsatisfiedLinkError e) {
-            e.printStackTrace();
-            Log.e(TAG, "could not link native handle for ts_detected_face_jni library!");
-        }
+        mHandle = native_create();
     }
 
     /**
@@ -52,9 +47,7 @@ public class FaceDetect {
      */
 
     public void uninitialize() {
-        if (mHandle != 0) {
-            native_destroy(mHandle);
-        }
+        native_destroy(mHandle);
     }
 
     /**
@@ -65,11 +58,6 @@ public class FaceDetect {
      * @return FaceInfo array if success, otherwise return null.
      */
     public FaceInfo[] dectectFeatures(Bitmap bmp) {
-        // check if the initialization failed
-        if (mHandle == 0) {
-            return null;
-        }
-
         int count = native_detect(mHandle, bmp);
         if (count < 1) {
             return null;
